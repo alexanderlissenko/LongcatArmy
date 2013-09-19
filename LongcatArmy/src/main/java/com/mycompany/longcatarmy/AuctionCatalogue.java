faf01a66cb23f485fda85b991843f0a80d4e536d
Emelie Svensson <sakuradoud@hotmail.com>
2013-09-19 17:03:12 +0200

    created oo-skel

---
+++ LongcatArmy/src/main/java/com/mycompany/longcatarmy/AuctionCatalogue.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.longcatarmy;
+
+import java.util.List;
+
+/**
+ *
+ * @author emesven
+ */
+public class AuctionCatalogue {
+    public List<Auction> auctionList;
+    public int auctionsExpired; //Date?********
+    
+    
+    
+    //Metoder?? inte klart??
+}
---
+++ LongcatArmy/src/main/java/com/mycompany/longcatarmy/User.java
@@ -0,0 +1,11 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.longcatarmy;
+
+
+public interface User {
+    
+    //nåt som vi inte har skrivit än
+}
