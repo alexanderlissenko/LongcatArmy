0ca58d1b41f0daeb90c6169aafbd76eebb706528
Alexander Lissenko <lissal@student.chalmers.se>
2013-09-23 11:17:02 +0200

    Filled in some methods in Auction.java, some methods still need completion.

--- LongcatArmy/src/main/java/com/mycompany/longcatarmy/Auction.java
+++ LongcatArmy/src/main/java/com/mycompany/longcatarmy/Auction.java
@@ -6,6 +6,7 @@ package com.mycompany.longcatarmy;
 
 import java.util.List;
 import java.util.Date;
+import java.util.HashMap;
 
 /**
  *
@@ -17,17 +18,31 @@ public class Auction {
     public Double price;
     public Date expireDate; //datum hur?
     public Customer creator;
-    public List<Customer> bidder;
+    public HashMap<Customer, Double> bidderMap; //för att kunna presentera listan på smidigt sätt
+    public List<HashMap> bidderList;
     public List<Customer> flagList;
     
     public Auction(Customer creator, String title, String info, Double price, Date expireDate){
-        
+        this.creator = creator;
+        this.title = title;
+        this.info = info;
+        this.price = price;
+        this.expireDate = expireDate;
     }
     
     public void addFlag(Customer c){
-        
+        //kolla sen när vi vet hur admin och customer ser ut
     }
-    public void setBid(Double price){
-        
+    
+    public void setBid(Customer bidder, Double price){
+        if(this.price < price) {
+            //eventuellt semaforer för att förhindra att två budar samtidigt?? kolla med handledare!
+            bidderMap.put(bidder, price);
+            bidderList.add(bidderMap);
+            this.price += price;
+        }
+        else {
+            //nåt felmeddelande, bud kan ej vara under aktuellt bud
+        }
     }
 }
