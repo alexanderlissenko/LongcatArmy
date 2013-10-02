e7ce883a4607d5910215d36ceb3791303939f975
Alexander Lissenko <lissal@student.chalmers.se>
2013-10-02 08:37:40 +0200

    fixed package path

--- LongcatArmy/src/main/java/longcatarmy/src/Customer.java
+++ LongcatArmy/src/main/java/longcatarmy/src/Customer.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.longcatarmy;
+package longcatarmy.src;
 
 import java.util.List;
 
