7168017422889f2c213f71731f79ec5486cd6b9f
k02sthe <stahlsparre@gmail.com>
2013-10-14 16:25:14 +0200

    fixed and trimmed fieldset

---
+++ LongcatArmy/src/main/java/longcatarmy/bb/FieldsetBean.java
@@ -0,0 +1,16 @@
+package longcatarmy.bb;
+import javax.faces.application.FacesMessage;  
+import javax.faces.context.FacesContext;  
+import org.primefaces.event.ToggleEvent; 
+
+/**
+ *
+ * @author k02sthe
+ */
+
+public class FieldsetBean {  
+    public void handleToggle(ToggleEvent event) {  
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Fieldset Toggled", "Visibility:" + event.getVisibility());  
+        FacesContext.getCurrentInstance().addMessage(null, msg);  
+    }  
+}
